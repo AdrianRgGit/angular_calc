b5dd889f9d999a4a068d77e1ae80258a64d0c02f
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-10-06 08:54:48 +0200

    finish calc

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-calc',
   templateUrl: './calc.component.html',
-  styleUrls: ['./calc.component.css']
+  styleUrls: ['./calc.component.css'],
 })
 export class CalcComponent {
+  n1: number = 0;
+  n2: number = 0;
+  sum: number = 0;
 
+  add() {
+    this.sum = this.n1 + this.n2;
+  }
+
+  reset() {
+    this.n1 = 0;
+    this.n2 = 0;
+    this.sum = 0;
+  }
 }
