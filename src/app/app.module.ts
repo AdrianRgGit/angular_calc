eadc23fc8d33a98faa435374cdf02d27d7ad496e
AdrianRgGit <adrianramirezgalera@gmail.com>
2023-10-09 09:33:40 +0200

    modul of res

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
 import { FormsModule } from '@angular/forms';
+import { ResComponent } from './res/res.component';
 
 @NgModule({
-  declarations: [AppComponent, CalcComponent],
+  declarations: [AppComponent, CalcComponent, ResComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
